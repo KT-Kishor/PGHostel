c23c9871a3254e61a68804cfe804c1d0158a879c
Pavan Kadam <pavankadam141@gmail.com>
2025-03-07 04:46:04 +0000

    First code

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("sap.kt.com.minihrsolution.controller.App", {
+      onInit() {
+      }
+  });
+});
---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/kt/com/minihrsolution/utils/validation"
+], (Controller,utils) => {
+    "use strict";
+
+    return Controller.extend("sap.kt.com.minihrsolution.controller.View1", {
+        onInit() {
+
+        }
+    });
+});
