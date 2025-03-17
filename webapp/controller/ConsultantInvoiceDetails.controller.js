257f4e8d2b1ff35a7002cf1b437979f53468ec71
amit3712 <kamit7102@gmail.com>
2025-03-17 18:06:11 +0530

    Validation of Contract invoice App

--- webapp/controller/ConsultantInvoiceDetails.controller.js
+++ webapp/controller/ConsultantInvoiceDetails.controller.js
@@ -1,116 +1,105 @@
-sap.ui.define([
+sap.ui.define(
+  [
     "./BaseController", //call base controller
     "sap/ui/model/json/JSONModel",
     "sap/m/MessageToast",
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator",
     "sap/m/MessagePopover",
-    "sap/m/MessageItem"
+    "sap/m/MessageItem",
+    "../utils/validation"
+  ],
+  function (
+    BaseController,JSONModel,MessageToast,Filter,FilterOperator,MessagePopover,MessageItem,utils
+  ) {
+    "use strict";
+    return BaseController.extend(
+      "sap.kt.com.minihrsolution.controller.ConsultantInvoiceDetails",
+      {
+        onInit: function () {
+          this.getRouter().getRoute("RouteNavConsultantInvoiceApplication").attachMatched(this._onRouteMatched, this);
+        },
+        _onRouteMatched: function (oEvent) {
+          var sPath = oEvent.getParameter("arguments").sPath;
+          var oPath = oEvent.getParameter("arguments").oPath;
+          this.decodedPath = decodeURIComponent(decodeURIComponent(sPath));
+          this.decodedEmployeeID = decodeURIComponent(oPath);
+          var oInvoiceModel = new sap.ui.model.json.JSONModel({
+            EmployeeID: "",
+            ConsultantName: "",
+            InvoiceTo: "",
+            InvoiceAddress: "",
+            InvoiceNo: "",
+            InvoiceDate: "",
+            ConsultantAddress: "",
+            GSTNO: "",
+            CompanyGSTNO: "",
+            MobileNo: "",
+            CGST: false,
+            SGST: false,
+            IGST: false,
+            BankName: "",
+            AccountName: "",
+            AccountNo: "",
+            IFSCCode: "",
+            PayBy: "",
+            GSTValid: false,
+            CGSTSelected: false,
+            IGSTSelected: false,
+            Percentage: "",
+            Currency: "INR",
+            Attachment: "",
+            name: "",
+            mimeType: "",
+          });
+          this.getView().setModel(oInvoiceModel, "ConsultantInvoiceModel");
 
-],
-    function (BaseController, JSONModel, MessageToast, Filter, FilterOperator, MessagePopover, MessageItem) {
-        "use strict";
-        return BaseController.extend("sap.kt.com.minihrsolution.controller.ConsultantInvoiceDetails", {
-            onInit: function () {
-                this.getRouter().getRoute("RouteNavConsultantInvoiceApplication").attachMatched(this._onRouteMatched, this);
-              },
-              _onRouteMatched: function (oEvent) {
-                // this.selectedContractID = null;
-                // this.commonLoginFunction("ConsultantInvoice");
-                // this.commonInvoiceSACFunction();
-                // this.LogoFunction();
-                // this.CommanCompanyEmailFunction("ContractApp");
-                // this.i18nModelMess = this.getView().getModel('i18n').getResourceBundle();
-                // var aDatePickerIds = ["idInDate", "idPaybyInv"];
-                // this._makeDatePickersReadOnly(aDatePickerIds);
-                var sPath = oEvent.getParameter("arguments").sPath;
-                var oPath = oEvent.getParameter("arguments").oPath;
-                this.decodedPath = decodeURIComponent(decodeURIComponent(sPath));
-                this.decodedEmployeeID = decodeURIComponent(oPath);
-                this.oModel = this.getOwnerComponent().getModel();
-                this.Discount = true;
-                this.UnitAmount = true;
-                // this.getView().getModel("loginModel").setProperty("/sendEmail", false);
-                // this.getView().getModel("loginModel").setProperty("/sendEmailForm", true);
-        
-                var oInvoiceModel = new sap.ui.model.json.JSONModel({
-                  EmployeeID: "",
-                  ConsultantName: "",
-                  InvoiceTo: "",
-                  InvoiceAddress: "",
-                  InvoiceNo: "",
-                  InvoiceDate: "",
-                  ConsultantAddress: "",
-                  GSTNO: "",
-                  CompanyGSTNO: "",
-                  MobileNo: "",
-                  CGST: false,
-                  SGST: false,
-                  IGST: false,
-                  BankName: "",
-                  AccountName: "",
-                  AccountNo: "",
-                  IFSCCode: "",
-                  PayBy: "",
-                  GSTValid: false,
-                  CGSTSelected: false,
-                  IGSTSelected: false,
-                  Percentage: "",
-                  Currency: "INR",
-                  Attachment: "",
-                  name: "",
-                  mimeType: "",
-                });
-                this.getView().setModel(oInvoiceModel, "ConsultantInvoiceModel");
-        
-                var oInvoiceItemModel = new sap.ui.model.json.JSONModel({
-                  SlNo: "",
-                  EmployeeID: "",
-                  Item: "",
-                  Days: "",
-                  SAC: "",
-                  UnitPrice: "",
-                  Total: "",
-                  SubTotal: "",
-                  TotalSum: "",
-                  Currency: "INR"
-                });
-                this.getView().setModel(oInvoiceItemModel, "oModelDataPro");
-        
-                var visibilityPlay = new sap.ui.model.json.JSONModel({ createVisi: true, editVisi: false, editable: true, invBtn: true, pasteBtn: true, merge: false, });
-                this.getView().setModel(visibilityPlay, "visiablityPlay");
-        
-                // // Get user type from the login model
-                // var sUserType = this.getView().getModel("loginModel").getProperty("/Type");
-        
-                // // Control visibility of ComboBox based on user type and whether we are in create or edit mode
-                // var oComboBox = this.getView().byId("idCont");
-        
-                // // Check if we're in edit mode: If sPath or oPath has specific values indicating edit mode
-                // var isEditMode = sPath !== "X" && oPath !== "Y"; // Modify this check if your edit condition is different
-        
-                // // If the user is "Admin" and not in edit mode, show the ComboBox, else hide it
-                // if (sUserType === "Admin" && !isEditMode) {
-                //   oComboBox.setVisible(true); // Show ComboBox if Admin and not in edit mode
-                // } else {
-                //   oComboBox.setVisible(false); // Hide ComboBox in edit mode or if not Admin
-                // }
-        
-                // if (sPath === "X" && oPath === "Y") {
-                //   this.readFunction("/ConsultantInvoice", "ConsultantInvoiceModel", true);
-                //   this.readFunction("/ConsultantInvoiceItem", "oModelDataPro", true);
-                //   this.byId("idTable").setMode("Delete");
-                //   this.byId("idSmartTableConsultantInvoiceItem").setModel(oInvoiceItemModel);
-                // } else {
-                //   this.fetchInvoiceData(this.decodedPath, this.decodedEmployeeID);
-                //   this.fetchInvoiceItems(this.decodedPath, this.decodedEmployeeID);
-                //   this.setVisibilityForEdit();
-                // }
-                // this.commonCurrencyFunction();
-                // this.scrollToSection("idNavConsultantInvoicePage", "idFirstSection");
-        
-                var oComboBox = this.getView().byId("idCont");
-                oComboBox.setSelectedKey("");
-              },
-        });
-    });
+          var oInvoiceItemModel = new sap.ui.model.json.JSONModel({
+            SlNo: "",
+            EmployeeID: "",
+            Item: "",
+            Days: "",
+            SAC: "",
+            UnitPrice: "",
+            Total: "",
+            SubTotal: "",
+            TotalSum: "",
+            Currency: "INR",
+          });
+          this.getView().setModel(oInvoiceItemModel, "oModelDataPro");
+
+          var visibilityPlay = new sap.ui.model.json.JSONModel({
+            createVisi: true,
+            editVisi: false,
+            editable: true,
+            invBtn: true,
+            pasteBtn: true,
+            merge: false,
+          });
+          this.getView().setModel(visibilityPlay, "visiablityPlay");
+        },
+        validateDate: function (oEvent) {
+          utils._LCvalidateDate(oEvent);
+        },
+        validateGstNumber: function (oEvent) {
+          utils._LCvalidateGstNumber(oEvent);
+        },
+        ValidateCommonFields: function (oEvent) {
+          utils._LCvalidateMandatoryField(oEvent);
+        },
+        validateMobileNo: function (oEvent) {
+          utils._LCvalidateMobileNumber(oEvent);
+        },
+        validateAccountNo: function (oEvent) {
+          utils._LCvalidateAccountNo(oEvent);
+        },
+        validateIfcCode: function (oEvent) {
+          utils._LCvalidateIfcCode(oEvent);
+        },
+        CI_onPressback: function () {
+          this.getRouter().navTo("RouteConsultantInvoiceApplication");
+        },
+      }
+    );
+  }
+);
