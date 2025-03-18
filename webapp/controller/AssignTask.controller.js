053b1911cd48ee5237cb8d206fb72a0fd33aafc9
Rani <rani.shinde@kalpavrikshatechnologies.com>
2025-03-18 19:59:04 +0530

    Ttainee certificate, onboard,Assign Task fragments

--- webapp/controller/AssignTask.controller.js
+++ webapp/controller/AssignTask.controller.js
@@ -1,15 +1,26 @@
 sap.ui.define([
-    "./BaseController", "sap/ui/model/json/JSONModel", "sap/m/MessageToast",
+    "./BaseController","../utils/validation", "sap/ui/model/json/JSONModel", "sap/m/MessageToast",
 ],
-    function (BaseController, JSONModel, MessageToast) {
+    function (BaseController,utils, JSONModel, MessageToast) {
         "use strict";
         return BaseController.extend("sap.kt.com.minihrsolution.controller.AssignTask", {
             onInit: function () {
                 this.getRouter().getRoute("RouteAssignTask").attachMatched(this._onRouteMatched, this);
             },
+            _onRouteMatched: function () {
+                this.i18nModel = this.getView().getModel("i18n").getResourceBundle();
+            },
+            validateDate: function (oEvent) {
+                utils._LCvalidateDate(oEvent);
+            },
+            ValidateCommonFields: function (oEvent) {
+                utils._LCvalidateMandatoryField(oEvent);
+            },
+
             AT_onPressback: function () {
                 this.getRouter().navTo("RouteManageAssignment");
             },
+            //open task fragment
             AT_onAssignEmpTask: function () {
                 if (!this.AT_oDialog) {
                     sap.ui.core.Fragment.load({
@@ -24,9 +35,25 @@ sap.ui.define([
                     this.AT_oDialog.open();
                 }
             },
-            TaskF_onTaskClose: function () {
+            NAF_onTaskClose: function () {
                 this.AT_oDialog.close();
             },
+            //Submit the task details
+            NAF_onSubmitTask: function () {
+                try {
+                    if ( utils._LCvalidateMandatoryField(sap.ui.getCore().byId("NA_id_TaskName"), "ID") && utils._LCvalidateMandatoryField(sap.ui.getCore().byId("NAF_id_Description"), "ID")
+                    && utils._LCvalidateDate(sap.ui.getCore().byId("NAF_id_StartDate"), "ID")&& utils._LCvalidateDate(sap.ui.getCore().byId("NAF_id_EndDate"), "ID") && utils._LCvalidateMandatoryField(sap.ui.getCore().byId("NAF_id_HoursWorked"),"ID")) {
+                        MessageToast.show(this.i18nModel.getText("taskAssignSucess"));
+                    }
+                    else {
+                        MessageToast.show(this.i18nModel.getText("mandetoryFields"));
+                    }
+                }
+                catch {
+                    MessageToast.show(this.i18nModel.getText("commonErrorMessage"));
+                }
+
+            }
 
 
 
--- webapp/controller/Trainee.controller.js
+++ webapp/controller/Trainee.controller.js
@@ -1,23 +1,100 @@
 sap.ui.define([
-    "./BaseController", "sap/ui/model/json/JSONModel", "sap/m/MessageToast",
+    "./BaseController", "../utils/validation", "sap/ui/model/json/JSONModel", "sap/m/MessageToast",
 ],
-    function (BaseController, JSONModel, MessageToast) {
+    function (BaseController, utils, JSONModel, MessageToast) {
         "use strict";
         return BaseController.extend("sap.kt.com.minihrsolution.controller.Trainee", {
             onInit: function () {
                 this.getRouter().getRoute("RouteTrainee").attachMatched(this._onRouteMatched, this);
             },
+            _onRouteMatched: function () {
+                this.i18nModel = this.getView().getModel("i18n").getResourceBundle();
+            },
+
+            ValidateCommonFields: function (oEvent) {
+                utils._LCvalidateMandatoryField(oEvent);
+            },
+            validateEmail: function (oEvent) {
+                utils._LCvalidateEmail(oEvent);
+            },
+
             T_onSignout: function () {
                 this.getRouter().navTo("RouteLoginPage");
             },
             T_onPressback: function () {
                 this.getRouter().navTo("RouteTilePage");
             },
-            T_onPressAddTrainee:function(){
-           this.getRouter().navTo("RouteTraineeDetails");
+            T_onPressAddTrainee: function () {
+                this.getRouter().navTo("RouteTraineeDetails");
+            },
+            //certificate fragment
+            T_onCertDownload: function () {
+                if (!this.TC_oDialog) {
+                    sap.ui.core.Fragment.load({
+                        name: "sap.kt.com.minihrsolution.fragment.TraineeCertificate",
+                        controller: this,
+                    }).then(function (TC_oDialog) {
+                        this.TC_oDialog = TC_oDialog;
+                        this.getView().addDependent(this.TC_oDialog);
+                        this.TC_oDialog.open();
+                    }.bind(this));
+                } else {
+                    this.TC_oDialog.open();
+                }
+            },
+            TCF_onPressCloseDialog: function () {
+                this.TC_oDialog.close();
+            },
+            //download certificate
+            TCF_onPressDownload: function () {
+                try {
+                    if (utils._LCvalidateMandatoryField(sap.ui.getCore().byId("TCF_id_ProjectName"), "ID")) {
+                        MessageToast.show(this.i18nModel.getText("downloadSucess"));
+                    }
+                    else {
+                        MessageToast.show(this.i18nModel.getText("mandetoryFields"));
+                    }
+                }
+                catch {
+                    MessageToast.show(this.i18nModel.getText("commonErrorMessage"));
+                }
+            },
+            //onboard trainee dialog
+            T_onOnboardPress: function () {
+                if (!this.TOb_oDialog) {
+                    sap.ui.core.Fragment.load({
+                        name: "sap.kt.com.minihrsolution.fragment.OnboardTrainee",
+                        controller: this,
+                    }).then(function (TOb_oDialog) {
+                        this.TOb_oDialog = TOb_oDialog;
+                        this.getView().addDependent(this.TOb_oDialog);
+                        this.TOb_oDialog.open();
+                    }.bind(this));
+                } else {
+                    this.TOb_oDialog.open();
+                }
+            },
+
+            OTF_onPressClose: function () {
+                this.TOb_oDialog.close();     
+            },
+            //onboard trainee
+            OTF_onPressOnboard:function(){
+                try {
+                    if (utils._LCvalidateEmail(sap.ui.getCore().byId("OTF_id_TraineeMail"), "ID")) {
+                        MessageToast.show(this.i18nModel.getText("traineeOnboardSucess"));
+                    }
+                    else {
+                        MessageToast.show(this.i18nModel.getText("mandetoryFields"));
+                    }
+                }
+                catch {
+                    MessageToast.show(this.i18nModel.getText("commonErrorMessage"));
+                }
+
             }
 
-         
-           
+
+
         });
     });
