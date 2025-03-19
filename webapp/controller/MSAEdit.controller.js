4953100b7e3213ac62e948dc74b827ccc7d4072e
Rani <rani.shinde@kalpavrikshatechnologies.com>
2025-03-20 00:08:46 +0530

    sow validation

--- webapp/controller/MSAEdit.controller.js
+++ webapp/controller/MSAEdit.controller.js
@@ -13,10 +13,51 @@ sap.ui.define([
             _onRouteMatched: function () {
                 this.i18nModel = this.getView().getModel("i18n").getResourceBundle();
             },
-            onPressback: function () {
+            MsaE_onBack: function () {
                 this.getOwnerComponent().getRouter().navTo("RouteMSA");
             },
-         
+
+            MsaE_onPressCreateSow: function () {
+                if (!this.SOW_oDialog) {
+                    sap.ui.core.Fragment.load({
+                        name: "sap.kt.com.minihrsolution.fragment.SowDetails",
+                        controller: this,
+                    }).then(function (SOW_oDialog) {
+                        this.SOW_oDialog = SOW_oDialog;
+                        this.getView().addDependent(this.SOW_oDialog);
+                        this.SOW_oDialog.open();
+                    }.bind(this));
+                } else {
+                    this.SOW_oDialog.open();
+                }
+            },
+            SOW_onCloseFrag: function () {
+                this.SOW_oDialog.close();
+            },
+            MsaE_validateDate: function (oEvent) {
+                utils._LCvalidateDate(oEvent);
+            },
+            MsaE_ValidateCommonFields: function (oEvent) {
+                utils._LCvalidateMandatoryField(oEvent);
+            },
+
+            SOW_onSubmitFrag: function () {
+                try {
+                    if (utils._LCvalidateMandatoryField(sap.ui.getCore().byId("SOW_id_MsaDesc"), "ID") && utils._LCvalidateDate(sap.ui.getCore().byId("SOW_id_StartDate"), "ID") && utils._LCvalidateDate(sap.ui.getCore().byId("SOW_id_EndDate"), "ID")) {
+                        MessageToast.show(this.i18nModel.getText("sowSuccess"));
+                    }
+                    else {
+                        MessageToast.show(this.i18nModel.getText("mandetoryFields"));
+                    }
+                }
+                catch {
+                    MessageToast.show(this.i18nModel.getText("commonErrorMessage"));
+                }
+
+            }
+
+
+
 
         });
     });
