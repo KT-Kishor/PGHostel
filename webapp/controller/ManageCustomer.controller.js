1682d33b1bc71e2a07fb761ecff34a3e19be06b6
amit3712 <kamit7102@gmail.com>
2025-03-19 19:20:58 +0530

    Manage Customer UI

--- webapp/controller/ManageCustomer.controller.js
+++ webapp/controller/ManageCustomer.controller.js
@@ -6,20 +6,27 @@ sap.ui.define([
     function (BaseController, JSONModel, MessageToast) {
         "use strict";
         return BaseController.extend("sap.kt.com.minihrsolution.controller.ManageCustomer", {
-            onInit: function () {
-                this.getRouter().getRoute("RouteManageCustomer").attachMatched(this._onRouteMatched, this);
-            },
+        onInit: function () {
+        this.getRouter().getRoute("RouteManageCustomer").attachMatched(this._onRouteMatched, this);
+        },
 
-            _onRouteMatched:function(oEvent){
-                this.getView().getModel("LoginModel").setProperty("/HeaderName", "Manage Customer"); 
-            },
+        _onRouteMatched:function(oEvent){
+        this.getView().getModel("LoginModel").setProperty("/HeaderName", "Customer Details"); 
+        },
 
-            onPressback: function () {
-                this.getOwnerComponent().getRouter().navTo("RouteTilePage");
-            },
-      
-            onLogout: function () {
-                this.getOwnerComponent().getRouter().navTo("RouteLoginPage");
-            },
-        });
-    });
+        //close the Dialog
+        onPressClose: function () {
+          this.oDialog.close();
+        },
+
+        onPressback: function () {
+          this.getOwnerComponent().getRouter().navTo("RouteTilePage");
+        },
+
+        onLogout: function () {
+          this.getOwnerComponent().getRouter().navTo("RouteLoginPage");
+        },
+      }
+    );
+  }
+);
