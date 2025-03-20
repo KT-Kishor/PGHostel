0c6a5c6179837be4b24454cbddfeed09850c16d4
Sai Behera <sai.j.behera@kalpavrikshatechnologies.com>
2025-03-20 19:07:45 +0530

    Schemeupload changes

--- webapp/controller/TilePage.controller.js
+++ webapp/controller/TilePage.controller.js
@@ -1,76 +1,84 @@
-sap.ui.define([
-    "./BaseController"],
-    function (BaseController) {
-        "use strict";
-        return BaseController.extend("sap.kt.com.minihrsolution.controller.TilePage", {
-            onInit: function () {
-                this.getRouter().getRoute("RouteTilePage").attachMatched(this._onRouteMatched, this);
-            },
-			TileV_onpressTrainee:function(){
-				this.getRouter().navTo("RouteTrainee");
-			},
-            TileV_onPressOffer: function () {
-                this.getRouter().navTo("RouteEmployeeOffer");
-            },
-            TileV_onpresslistofholidays: function () {
-                this.getRouter().navTo("RouteListofholidays",{Year:"Listofholidays"})
-            },
-            TileV_onpressIDCARD: function () {
-                this.getRouter().navTo("RouteIDCardApplication")
-            },
-            TileV_onpressLeave: function () {
-                this.getRouter().navTo("RouteAdminApplyLeave")
-            },  
-            TileV_onpressConsultantInvoice:function(){
-                this.getRouter().navTo("RouteConsultantInvoiceApplication")
-            },
-            TileV_onpressContract: function () {
-                this.getRouter().navTo("RouteContract")
-            },
-            TileV_onPressAdminPaySlip: function () {
-                this.getRouter().navTo("RouteAdminPaySlip");
-            },
-            TileV_onpressSelfservice: function () {
-                this.getRouter().navTo("RouteSelfService");
-            },
-            TileV_onpressInbox: function(){
-                this.getRouter().navTo("RouteMyInbox",{sMyInBox:"MyInboxView"});
-            },
-            TileV_onpressInvoiceApp: function () {
-                this.getRouter().navTo("RouteCompanyInvoice",{sPath:"Invoice"});
-            },
-            TileV_onpressQuotation: function () {
-                this.getRouter().navTo("RouteQuotation");
-            },
-            TileV_onpressAssignment:function(){
-                this.getRouter().navTo("RouteManageAssignment");
-            },
-            TileV_onpresstimesheet:function(){
-                this.getRouter().navTo("RouteTimesheet");
-            },
-            TileV_onPressTimesheetApp:function(){
-                this.getRouter().navTo("RouteTimesheetApproval")
-            },
-            TileV_onPressGenerateSalary: function () {
-                this.getRouter().navTo("RouteGenerateSalary");
-            },
-            TileV_onPressManagePayroll: function () {
-                this.getRouter().navTo("RouteManagePayroll");
-            },
-            TileV_onpressEmployeeDetails: function () {
-                this.getRouter().navTo("RouteEmployeeDetails");
-            },
-            TileV_onBackPress:function(){
-                this.getRouter().navTo("RouteLoginPage");
-            },
-            TileV_onpressAddCustomer:function(){
-                this.getRouter().navTo("RouteManageCustomer");
-            },
-            TileV_onpressMSA:function(){
-                this.getRouter().navTo("RouteMSA")
-            },
-            TileV_onpressExpenseApp:function(){
-                this.getRouter().navTo("RouteExpensePage")
-            }
+sap.ui.define(["./BaseController"], function (BaseController) {
+  "use strict";
+  return BaseController.extend(
+    "sap.kt.com.minihrsolution.controller.TilePage",
+    {
+      onInit: function () {
+        this.getRouter()
+          .getRoute("RouteTilePage")
+          .attachMatched(this._onRouteMatched, this);
+      },
+      TileV_onpressTrainee: function () {
+        this.getRouter().navTo("RouteTrainee");
+      },
+      TileV_onPressOffer: function () {
+        this.getRouter().navTo("RouteEmployeeOffer");
+      },
+      TileV_onpresslistofholidays: function () {
+        this.getRouter().navTo("RouteListofholidays", {
+          Year: "Listofholidays",
         });
-    });
+      },
+      TileV_onpressIDCARD: function () {
+        this.getRouter().navTo("RouteIDCardApplication");
+      },
+      TileV_onpressLeave: function () {
+        this.getRouter().navTo("RouteAdminApplyLeave");
+      },
+      TileV_onpressConsultantInvoice: function () {
+        this.getRouter().navTo("RouteConsultantInvoiceApplication");
+      },
+      TileV_onpressContract: function () {
+        this.getRouter().navTo("RouteContract");
+      },
+      TileV_onPressAdminPaySlip: function () {
+        this.getRouter().navTo("RouteAdminPaySlip");
+      },
+      TileV_onpressSelfservice: function () {
+        this.getRouter().navTo("RouteSelfService");
+      },
+      TileV_onpressInbox: function () {
+        this.getRouter().navTo("RouteMyInbox", { sMyInBox: "MyInboxView" });
+      },
+      TileV_onpressInvoiceApp: function () {
+        this.getRouter().navTo("RouteCompanyInvoice", { sPath: "Invoice" });
+      },
+      TileV_onpressQuotation: function () {
+        this.getRouter().navTo("RouteQuotation");
+      },
+      TileV_onpressAssignment: function () {
+        this.getRouter().navTo("RouteManageAssignment");
+      },
+      TileV_onpresstimesheet: function () {
+        this.getRouter().navTo("RouteTimesheet");
+      },
+      TileV_onPressTimesheetApp: function () {
+        this.getRouter().navTo("RouteTimesheetApproval");
+      },
+      TileV_onPressGenerateSalary: function () {
+        this.getRouter().navTo("RouteGenerateSalary");
+      },
+      TileV_onPressManagePayroll: function () {
+        this.getRouter().navTo("RouteManagePayroll");
+      },
+      TileV_onpressEmployeeDetails: function () {
+        this.getRouter().navTo("RouteEmployeeDetails");
+      },
+      TileV_onBackPress: function () {
+        this.getRouter().navTo("RouteLoginPage");
+      },
+      TileV_onpressAddCustomer: function () {
+        this.getRouter().navTo("RouteManageCustomer");
+      },
+      TileV_onpressMSA: function () {
+        this.getRouter().navTo("RouteMSA");
+      },
+      TileV_onpressExpenseApp: function () {
+        this.getRouter().navTo("RouteExpensePage");
+      },
+      TileV_onPressManageSchemeUpload: function () {
+        this.getRouter().navTo("RouteSchemeUpload");
+      },
+    }
+  );
+});
