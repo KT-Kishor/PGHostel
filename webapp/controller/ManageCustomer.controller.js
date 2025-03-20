c103514e81848fa7762bdbaa7ca95fd1b9b2b8e6
amit3712 <kamit7102@gmail.com>
2025-03-20 11:03:08 +0530

    Shivang changes

--- webapp/controller/ManageCustomer.controller.js
+++ webapp/controller/ManageCustomer.controller.js
@@ -1,9 +1,10 @@
 sap.ui.define([
     "./BaseController", //call base controller
     "sap/ui/model/json/JSONModel",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "../utils/validation"
 ],
-    function (BaseController, JSONModel, MessageToast) {
+    function (BaseController, JSONModel, MessageToast,utils) {
         "use strict";
         return BaseController.extend("sap.kt.com.minihrsolution.controller.ManageCustomer", {
         onInit: function () {
@@ -14,8 +15,93 @@ sap.ui.define([
         this.getView().getModel("LoginModel").setProperty("/HeaderName", "Customer Details"); 
         },
 
+        MC_onAddCustomerDetails: function () {
+          var oView = this.getView();
+          var oModel;
+          var JSONModel = {
+            CompanyName: "",
+            Name: "",
+            PAN: "",
+            GST: "",
+            Address: "",
+            MailID: "",
+            MobileNo: "",
+            LUT: "",
+            Type: "",
+            Value: "0",
+            Salutation: "Mr.",
+            CustomerEmail: ""
+          };
+          oModel = new sap.ui.model.json.JSONModel(JSONModel);
+          this.getView().setModel(oModel, "CreateCustomerModel");
+          this.openLeaveDialog(oView);
+        },
+
+        // Open the leave dialog fragment
+        openLeaveDialog: function (oView) {
+          if (!this.oDialog) {
+            sap.ui.core.Fragment.load({
+              name: "sap.kt.com.minihrsolution.fragment.CreateCustomer",
+              controller: this
+            }).then(function (oDialog) {
+              this.oDialog = oDialog;
+              oView.addDependent(this.oDialog);
+              this.oDialog.open();
+            }.bind(this));
+          } else {
+            this.oDialog.open();
+          }
+        },
+
+        onRadioButtonChange: function () {
+          if (sap.ui.getCore().byId("MC_id_groupCustGst").getSelectedIndex() === 0) {
+            this.getView().getModel("CreateCustomerModel").setProperty('/Value', '9');
+            this.getView().getModel("CreateCustomerModel").setProperty('/Type', "CGST/SGST");
+          } else {
+            this.getView().getModel("CreateCustomerModel").setProperty('/Value', '18');
+            this.getView().getModel("CreateCustomerModel").setProperty('/Type', "IGST");
+          }
+        },
+
+        MC_ValidateCommonFields: function (oEvent) {
+          utils._LCvalidateMandatoryField(oEvent);
+        },
+
+        MC_ValidateGstNumber: function (oEvent) {
+          utils._LCvalidateGstNumber(oEvent);
+        },
+
+        MC_ValidateMobileNo: function (oEvent) {
+          utils._LCvalidateMobileNumber(oEvent);
+        },
+
+        MC_ValidatePanCard: function (oEvent) {
+          utils._LCvalidatePanCard(oEvent);
+        },
+
+        MC_ValidateEmail: function (oEvent) {
+        utils._LCvalidateEmail(oEvent);
+        },
+
+        MC_onPressSubmit: function () {
+          try {
+            if (
+              utils._LCvalidateGstNumber(sap.ui.getCore().byId("MC_id_CustomGst"), "ID") &&
+              utils._LCvalidateMandatoryField(sap.ui.getCore().byId("MC_id_CustCompanyName"), "ID") && 
+              utils._LCvalidateMandatoryField(sap.ui.getCore().byId("MC_id_CustCustomerName"), "ID") && 
+              utils._LCvalidatePanCard(sap.ui.getCore().byId("MC_id_CustomPan"), "ID") &&
+              utils._LCvalidateEmail(sap.ui.getCore().byId("MC_id_CustMail"), "ID") &&  utils._LCvalidateEmail(sap.ui.getCore().byId("MC_id_FinanceEmail"), "ID") &&
+              utils._LCvalidateMobileNumber(sap.ui.getCore().byId("MC_id_CustMob"), "ID") && utils._LCvalidateMandatoryField(sap.ui.getCore().byId("MC_id_CustAddress"), "ID") ) {
+            } else {
+                sap.m.MessageToast.show("Make sure all the mandatory fields are filled and validate the entered value");
+            }
+        } catch (error) {
+            sap.m.MessageToast.show("Technical error, please contact the administrator");
+        }
+      },
+
         //close the Dialog
-        onPressClose: function () {
+        MC_onPressClose: function () {
           this.oDialog.close();
         },
 
