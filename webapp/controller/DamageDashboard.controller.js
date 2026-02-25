8e93cacefea9eb46adc614cd97cf48f4dd2be5eb
bhagya294 <bhagyaspatil2000@gmail.com>
2026-02-25 13:09:05 +0530

    filter changes in DD

--- webapp/controller/DamageDashboard.controller.js
+++ webapp/controller/DamageDashboard.controller.js
@@ -12,6 +12,40 @@ sap.ui.define([
             this.getOwnerComponent().getRouter().getRoute("RouteDamageDashboard").attachMatched(this._onRouteMatched, this);
         },
 
+        _onRouteMatched: async function () {
+            sap.ui.core.BusyIndicator.show(0);
+            try {
+                this.commonLoginFunction();
+                this.i18nModel = this.getView().getModel("i18n").getResourceBundle();
+                this._ViewDatePickersReadOnly(["id_DD_year"], this.getView());
+                const oLogin = this.getOwnerComponent().getModel("LoginModel")?.getData();
+                if (!oLogin || !oLogin.BranchCode) return sap.m.MessageToast.show("Login branch not found");
+                this._aUserBranches = oLogin.BranchCode ? oLogin.BranchCode.split(",").map(b => b.trim()) : [];
+                await this._loadUserBranches();
+            } catch (err) {
+                MessageToast.show("Something went wrong");
+            } finally {
+                sap.ui.core.BusyIndicator.hide();
+            }
+        },
+
+        _loadUserBranches: async function () {
+            const oData = await this.ajaxReadWithJQuery("HM_Branch", {});
+            let aAllBranches = Array.isArray(oData.data) ? oData.data : [oData.data];
+            let aFiltered = aAllBranches.filter(b =>
+                this._aUserBranches.includes(b.BranchID));
+            this.getView().setModel(new JSONModel(aFiltered), "branchModel");
+        },
+
+        DD_onPressClear: function () {
+            this.byId("id_DD_branch").setValue("");
+            this.byId("id_DD_year").setValue("");
+        },
+
+        DD_search: function () {
+
+        },
+
         onHome: function () {
             this.CommonLogoutFunction();
         },
